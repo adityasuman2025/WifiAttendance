881a0bef4e0a8115b7fd4855994cbd9ffc60ec3f
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-02-27 19:06:28 +0530

    next generation started

--- app/src/main/java/com/example/professorattendance/Dashboard.java
+++ app/src/main/java/com/example/professorattendance/Dashboard.java
@@ -1,15 +1,43 @@
 package com.example.professorattendance;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
 {
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        activityManager.moveTaskToFront(getTaskId(), 0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+            return false;
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG );
+        super.onAttachedToWindow();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
